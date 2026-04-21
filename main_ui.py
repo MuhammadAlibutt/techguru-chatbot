7ad8bda9cc0a09a45a7bb24ae111877c8b7fc04f
Muhammad Ali <muhammadalibutt34@gmail.com>
2026-04-21 16:41:09 +0100

    Fix path for streamlit 2

--- main_ui.py
+++ main_ui.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import os 
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+
 from src.services.azure_services import TechAgent
 
 
